7d2bd0ef467302081dbdc1529ed353bff5846a4a
Tahlil1 <bsse0803@iit.du.ac.bd>
2019-10-19 08:56:46 +0600

    copy column works

--- aes.js
+++ aes.js
@@ -14,15 +14,37 @@ const createStateMatrix = function(hexList) {
   return matrix;
 }
 
-const keyHexList = [0x54, 0x68, 0x61, 0x74, 0x73, 0x20, 0x6D, 0x79, 0x20, 0x4B, 0x75, 0x6E, 0x67, 0x20, 0x46, 0x75];
+copyColumn = function(mat, pos, col) {
+  for (i = 0; i < col.length; i++) {
+    mat[i][pos] = col[i];
+  }
+  return mat;
+}
+
+addRoundKey = function(currentRound) {
+  // byte[] col;
+  // int j = 0;
+  // for (int i = 0; i < 4; i++) {
+  //   col = xorBytes(getColumn(state, i), getColumn(keys, currentRound*4+j));
+  //   columnCopy(state, i, col);
+  //   j++;
+  // }
+}
+
+const keyHexList = [0x54, 0x68, 0x61, 0x74, 0x73, 0x20, 0x6D, 0x79, 0x20, 0x4B, 0x75, 0x6E, 0x67, 0x20, 0x46, 0x750];
 const plainHexList = [0x54, 0x77, 0x6F, 0x20, 0x4F, 0x6E, 0x65, 0x20, 0x4E, 0x69, 0x6E, 0x65, 0x20, 0x54, 0x77, 0x6F];
 console.log("Test input:");
 console.log(plainHexList);
 console.log("Test key:");
 console.log(keyHexList);
+
 let matrixKey = createStateMatrix(keyHexList);
 let matrixInput = createStateMatrix(plainHexList);
-console.log("Test key:");
+
+console.log("Test 1st key:");
 console.log(matrixKey);
 console.log("Test input:");
 console.log(matrixInput);
+console.log(0x12 ^ 0x23);
+//matrixKey = copyColumn(matrixKey, 4, [1,2,3,4])
+//console.log(matrixKey);
